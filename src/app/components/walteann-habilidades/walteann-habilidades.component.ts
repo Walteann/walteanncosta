9e5cb09ad6a8638bb707fa6dd334ec9e7914608c
Walteann Costa <walteann.costa.silva@accenture.com>
2018-08-26 16:52:08 -0300

    refatoracao component habilidades

--- src/app/components/walteann-habilidades/walteann-habilidades.component.ts
+++ src/app/components/walteann-habilidades/walteann-habilidades.component.ts
@@ -1,15 +1,100 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-walteann-habilidades',
-  templateUrl: './walteann-habilidades.component.html',
-  styleUrls: ['./walteann-habilidades.component.css']
+    selector: 'app-walteann-habilidades',
+    templateUrl: './walteann-habilidades.component.html',
+    styleUrls: ['./walteann-habilidades.component.css']
 })
-export class WalteannHabilidadesComponent implements OnInit {
+export class WalteannHabilidadesComponent {
 
-  constructor() { }
+    conhecimentos: any[] = [
+        {
+            alt: 'php',
+            img: 'assets/imagem/skills/java.png'
+        },
+        {
+            alt: 'java',
+            img: 'assets/imagem/skills/php.png'
+        },
+        {
+            alt: 'angularjs',
+            img: 'assets/imagem/skills/angularjs.png'
+        },
+        {
+            alt: 'csharp',
+            img: 'assets/imagem/skills/csharp.png'
+        },
+        {
+            alt: 'html5',
+            img: 'assets/imagem/skills/html5.png'
+        },
+        {
+            alt: 'css3',
+            img: 'assets/imagem/skills/css3.png'
+        },
+        {
+            alt: 'javascript',
+            img: 'assets/imagem/skills/javascript.png'
+        },
+        {
+            alt: 'ecmascript6',
+            img: 'assets/imagem/skills/es6.png'
+        },
+        {
+            alt: 'jquery',
+            img: 'assets/imagem/skills/jquery.png'
+        },
+        {
+            alt: 'mysql',
+            img: 'assets/imagem/skills/mysql.png'
+        },
+        {
+            alt: 'bootstrap3',
+            img: 'assets/imagem/skills/bootstrap3.png'
+        },
+        {
+            alt: 'materialize',
+            img: 'assets/imagem/skills/materialize.png'
+        },
+        {
+            alt: 'photoshop',
+            img: 'assets/imagem/skills/photoshop.png'
+        },
+    ];
 
-  ngOnInit() {
-  }
+    familiarizado: any[] = [
+        {
+            alt: 'git',
+            img: 'assets/imagem/skills/git.png'
+        },
+        {
+            alt: 'github',
+            img: 'assets/imagem/skills/github.png'
+        },
+        {
+            alt: 'filezilla',
+            img: 'assets/imagem/skills/filezilla.png'
+        },
+        {
+            alt: 'tortoisesvn',
+            img: 'assets/imagem/skills/tortoisesvn.png'
+        },
+        {
+            alt: 'sql',
+            img: 'assets/imagem/skills/sql.png'
+        },
+        {
+            alt: 'xampp',
+            img: 'assets/imagem/skills/xampp.png'
+        },
+        {
+            alt: 'trello',
+            img: 'assets/imagem/skills/trello.png'
+        },
+        {
+            alt: 'moqups',
+            img: 'assets/imagem/skills/moqups.png'
+        },
+    ];
 
 }
